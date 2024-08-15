1f750f18a41e408670b132056630597b1eb7620b
AilenAlbornoz <albornozailen555@gmail.com>
2024-08-14 21:06:34 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('button');
     button.addEventListener('click', (event) => { // Manejador para el botón
         event.stopPropagation(); // Evita que el evento se propague al div
-        saludar();// Llama a la función saludar
     });
 
     document.querySelector('div').addEventListener('click', () => {
